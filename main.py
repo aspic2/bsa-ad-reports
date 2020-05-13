8a9b6bda75e0fc39ef2e216022f23d8a1da665e3
thomp <thomp@protonmail.com>
2020-05-13 15:26:32 -0500

    tidying up

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ from app.reader import Reader, FileManager, ZipFileManager
 
 def main():
     credentials = Credentials().get()
+
     email_body = GmailApi(credentials).build_service().get_latest_message_body()
     download_link = Reader(email_body).get_download_link()
 
-    # download the zip file
     download_response = requests.get(download_link)
     zip_file = ZipFileManager("downloaded.zip").write_content(download_response.content)
     report_csv = zip_file.read_first_file().return_content_as_stripped_string()
     report_csv_file = FileManager("report.csv").write_content(report_csv)
 
     formatted_reporting_data = [data.split(',') for data in Reader(report_csv).return_content_as_list()]
-
     updated_spreadsheet = SheetsApi(credentials, SpreadsheetMetadata()).build_service().write_to_spreadsheet(formatted_reporting_data)
 
     print("main.py successfully completed")
