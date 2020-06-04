d9bc1ddfaaee785526a84355cc415b34bfd3cbd0
thomp <thomp@protonmail.com>
2020-06-04 09:58:24 -0500

    added bsa_api_keys. Shell file, not actual keys.

---
+++ resources/bsa_api_keys.py
@@ -0,0 +1,6 @@
+bsa_api_keys = {"advertiser_1_name": "key",
+                "advertiser_2_name": "key",
+                "advertiser_3_name": "key",
+                "advertiser_4_name": "key",
+                "advertiser_5_name": "key"
+               }
