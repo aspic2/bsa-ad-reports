ac8a748733f0b3b827c9ce8bfae6cfc5d435553e
thomp <thomp@protonmail.com>
2020-06-05 10:39:07 -0500

    added __main__ functionality to module

--- first_party_reports.py
+++ first_party_reports.py
@@ -1,6 +1,13 @@
+from resources.bsa_api_keys import bsa_api_keys
+
 from app.bsa_api_connections import LineItems
 import random
 
 def first_party_reports():
-    advertiser = random.choice(bsa_api_keys.keys())
+    advertiser = random.choice(list(bsa_api_keys.keys()))
     data = LineItems(advertiser).get_json_response()
+    print("Advertiser = {}".format(advertiser))
+    print(data)
+
+if __name__ == '__main__':
+    first_party_reports()
