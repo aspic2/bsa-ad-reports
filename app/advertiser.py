b270bc63ac3e378bf2834efd6db277cb8f0af35e
thomp <thomp@protonmail.com>
2020-06-08 11:30:45 -0500

    rename module to use standard lowercase convention

---
+++ app/advertiser.py
@@ -0,0 +1,28 @@
+from resources.advertisers_info import advertisers_info
+
+class Advertiser(object):
+
+    def __init__(self, info):
+        self.name = info.get("name")
+        self.bsa_api_key = info.get("api_key")
+        self.spreadsheet_id = info.get("spreadsheet_id")
+        self.tab_name = info.get("tab_name")
+        self.stats = None
+
+    def set_stats(self):
+        self.stats = gather_and_calculate_stats()
+        return self
+
+    def get_stats(self):
+        return self.stats
+
+    def get_api_key(self):
+        return self.bsa_api_key
+
+    def get_name(self):
+        return self.name
+
+
+
+if __name__ == '__main__':
+    main()
