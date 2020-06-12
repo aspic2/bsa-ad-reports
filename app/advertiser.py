4a8e3d8df2411a05bc6c3a2223c0e58b18d52ef5
thomp <thomp@protonmail.com>
2020-06-12 10:43:00 -0500

    added more attributes to handle 1st and 3rd party needs. changed range to a parameter in get_range()

--- app/advertiser.py
+++ app/advertiser.py
@@ -4,9 +4,13 @@ class Advertiser(object):
 
     def __init__(self, info):
         self.name = info.get("name")
+        # TODO: Plenty of unused attributes here. Maybe subclass them?
         self.bsa_api_key = info.get("api_key")
         self.spreadsheet_id = info.get("spreadsheet_id")
         self.tab_name = info.get("tab_name")
+        self.third_party_tab_name = info.get("third_party_tab_name")
+        self.email_subject = info.get("email_subject")
+        self.email_attachment_name = info.get("email_attachment_name")
         self.stats = None
 
     def set_stats(self):
@@ -25,9 +29,9 @@ class Advertiser(object):
     def get_spreadsheet_id(self):
         return self.spreadsheet_id
 
-    def get_range(self):
+    def get_range(self, range="!B1:Z"):
         # default to leaving column A open
-        return self.tab_name + "!B1:Z"
+        return self.tab_name + range
 
 
 
