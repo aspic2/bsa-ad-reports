5c79124dad46d226aa9bef0a568e68017162bc55
thomp <thomp@protonmail.com>
2020-12-24 20:56:40 -0600

    adding file to store the campaigns list sheet id and range

---
+++ resources/confidential.py
@@ -0,0 +1,11 @@
+
+campaigns_list_sheet_id = "your_sheet_id_here"
+campaigns_list_range = "your_tab_name!and_range1:here2"
+
+campaigns_list_hash = {"id": campaigns_list_sheet_id,
+    "range": campaigns_list_range
+    }
+
+
+if __name__ == '__main__':
+    main()
