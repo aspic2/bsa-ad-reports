f5155af2a713a1a553edf3dbb9a00b102e60f5ff
thomp <thomp@protonmail.com>
2020-12-24 21:47:01 -0600

    removed resources/advertisers_info.py

--- resources/advertisers_info.py
+++
@@ -1,8 +0,0 @@
-advertisers_info = [{"name": "[advertiser_1]",
-        "api_key": "[key_1]",
-        "spreadsheet_id": "[id_1]",
-        "tab_name": "import_bsa"},
-       {"name": "[advertiser_2]",
-        "api_key": "[key_2]",
-        "spreadsheet_id": "[id_2]",
-        "tab_name": "import_bsa"}]
